de60a6dda1153c2ebf33c2a53a13eef8e9e9d505
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-09-06 16:01:32 -0500

    Initialize the sphinx project

---
+++ dl4s_book/conf.py
@@ -0,0 +1,29 @@
+from importlib.metadata import version
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Deep Learning Reuse For Science"
+copyright = "2025, Nicholas M. Synovic"
+author = "Nicholas M. Synovic"
+release = version(distribution_name="dl4s_book")
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
---
+++ dl4s_book/index.rst
@@ -0,0 +1,16 @@
+.. Deep Learning Reuse For Science documentation master file, created by
+   sphinx-quickstart on Sat Sep  6 15:56:57 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Deep Learning Reuse For Science documentation
+=============================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
